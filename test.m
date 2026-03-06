c5da6372016cc4ca0364c1044f31fc0fc9024632
J-Carrig <jcarrig@tcd.ie>
2026-03-06 16:56:32 +0000

    file cleanup
    w

--- test.m
+++
@@ -1,6 +0,0 @@
-try
-    s = serialport("COM9",115200)
-    disp("Connected!");
-catch ME
-    disp(ME.message)
-end
--- tf_testing.m
+++
@@ -1,8 +0,0 @@
-s = tf('s');
-K = 126;   % max RPM
-tau = 0.1; % example time constant
-Ts = 0.01; % 10 ms sample time
-
-G = K/(tau*s + 1);
-Gd = c2d(G, Ts, 'tustin');   % or 'zoh'
-[num, den] = tfdata(Gd, 'v');  % returns vectors
